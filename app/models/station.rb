f561c550b007c42b9a6cda70ceddab93c333b1d1
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-12-02 09:49:06 -0700

    Adds one to many relationship city and stations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,3 @@
 class Station < ActiveRecord::Base
-
+  belongs_to :city
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202162839) do
+ActiveRecord::Schema.define(version: 20161202164254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20161202162839) do
     t.string   "installation_date"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "city_id"
   end
 
 end
