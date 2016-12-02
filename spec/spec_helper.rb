485e4753dfeb497b05a243f0eb4165210864f695
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-12-02 09:03:59 -0700

    Adds database cleaner to spec_helper and gem file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,21 @@ ENV["RACK_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
 require 'capybara/dsl'
+require 'database_cleaner'
 
 Capybara.app = BikeShareApp
 
+DatabaseCleaner.strategy = :truncation
+
 RSpec.configure do |c|
+  c.before(:all) do
+    DatabaseCleaner.clean
+  end
+
+  c.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   c.include Capybara::DSL
 end
+
