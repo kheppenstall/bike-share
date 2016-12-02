1a1e28cc8eadde447046666dedbd31025f33b70b
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-12-02 09:32:11 -0700

    Adds cities and stations tables and city.rb and station.rb

---
+++ db/migrate/20161202161837_create_stations.rb
@@ -0,0 +1,11 @@
+class CreateStations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stations do |t|
+      t.string :name
+      t.integer :dock_count
+      t.string :installation_date
+
+      t.timestamps null: false
+    end
+  end
+end
