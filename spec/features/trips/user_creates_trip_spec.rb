751cd55c83c0b7119bf1c8dabdcb5a67fdb23a23
Drew Palazzari <drewpalazzari@hotmail.com>
2016-12-05 15:43:39 -0700

    adds create function for trips

--- spec/features/trips/user_creates_trip_spec.rb
+++ spec/features/trips/user_creates_trip_spec.rb
@@ -1,9 +1,45 @@
 require './spec/spec_helper'
 
 describe "user visits /trips/new" do
-  it "user sees create message" do
+  it "and sees create message" do
     visit('/trips/new')
 
     expect(page).to have_content("Create a Trip")
+  end
+
+  it "and sees form" do
+    visit('/trips/new')
+
+    expect(page).to have_content("Start Station: ")
+    expect(page).to have_content("Start Date: ")
+    expect(page).to have_content("End Station: ")
+    expect(page).to have_content("End Date: ")
+    expect(page).to have_content("Bike ID: ")
+    expect(page).to have_content("Zip Code: ")
+  end
+
+  it "and creates a new trip" do
+    station   = Station.create(name: "Station", dock_count: 20, installation_date: "02/01/1990", city_id: 1)
+    subtype   = SubscriptionType.create(name: "Customer")
+    condition = Condition.create(date: "01/02/2016")
+
+    visit('/trips/new')
+
+    fill_in "trip[bike_id]", with: 14
+    fill_in "trip[zip_code]", with: 80918
+    # fill_in "trip[start_station_name]", with: "Station"
+    fill_in "trip[start_date]", with: "01/02/2016 01:00:00"
+    # fill_in "trip[end_station_name]", with: "Station"
+    fill_in "trip[end_date]", with: "01/02/2016 01:01:00"
+
+    click_on("Submit")
+
+    save_and_open_page
+    expect(current_path).to eq("/trips")
+    expect(page).to have_content("Station")
+    # expect(page).to have_content(14)
+    # expect(page).to have_content(80918)
+    expect(page).to have_content("01/02/2016")
+    expect(page).to have_content("01/02/2016")
   end
 end
