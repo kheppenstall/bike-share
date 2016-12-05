0c8ce93e68422e417ef8358f819fde3e06ad9e73
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-12-05 16:03:37 -0700

    Start condition seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'time'
 require './db/parser'
 require './app/models/station'
+require './app/models/condition'
 require './app/models/city'
 include CSV_parser
 
@@ -13,4 +14,17 @@ stations = parse('./db/csv/station.csv').map do |row|
                  installation_date: Date.strptime(row[:installation_date], "%m/%d/%Y"),
                  city_id:           city.id
                  )
+end
+
+conditions = parse('./db/csv/weather.csv').map do |row|
+
+Condition.create(date: Date.strptime(row[:date], "%m/%d/%Y"),
+                 max_temperature: row[:max_temperature],
+                 mean_temperature: row[:mean_temperature],
+                 min_temperature: row[:min_temperature],
+                 mean_humidity: row[:mean_humidity],
+                 mean_visibility: row[:mean_visibility],
+                 mean_wind_speed: row[:mean_wind_speed],
+                 precipitation: row[:precipitation]
+                )
 end
