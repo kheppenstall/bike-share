72f02fbc5c3744d247e380474e53aff820ccf202
Drew Palazzari <drewpalazzari@hotmail.com>
2016-12-05 13:27:04 -0700

    everything up to date

--- spec/features/trips/user_creates_trip_spec.rb
+++ spec/features/trips/user_creates_trip_spec.rb
@@ -4,8 +4,6 @@ describe "user visits /trips/new" do
   it "user sees create message" do
     visit('/trips/new')
 
-    save_and_open_page
-
     expect(page).to have_content("Create a Trip")
   end
 end
