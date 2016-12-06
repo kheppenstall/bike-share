f971cecaec1209879963543e1eb9488dfe97cbbb
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-12-05 19:55:12 -0700

    Change seed features.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 require 'time'
+require 'date'
 require './db/parser'
 require './app/models/station'
 require './app/models/condition'
 require './app/models/city'
+require './app/models/subscription_type'
+require './app/models/trip'
+
 include CSV_parser
 
 stations = parse('./db/csv/station.csv').map do |row|
@@ -29,15 +33,26 @@ conditions = parse('./db/csv/weather.csv').map do |row|
                   )
 end
 
-trips = parse('./db/csv/trip.csv').map do |row|
+trips = parse('./db/csv/trip-fixture.csv').map do |row|
 
   subscription_type = SubscriptionType.find_or_create_by(name: row[:subscription_type])
   start_station = Station.find_by(name: row[:start_station_name])
-  start_station = Station.find_by(name: row[:end_station_name])
-  start_date = Date.strptime(row[:start_date], "%m/%d/%Y %H:%M")
-  condition = Condition.find_by(date: :start_date)
+  start_station_id = start_station.id rescue nil
+  end_station = Station.find_by(name: row[:end_station_name])
+  end_station_id = end_station.id rescue nil
+  end_date = Date.strptime(row[:end_date], "%m/%d/%Y %H:%M").strftime("%m/%d/%Y")
+  start_date = Date.strptime(row[:start_date], "%m/%d/%Y %H:%M").strftime("%m/%d/%Y")
+  condition = Condition.find_by(date: start_date)
   condition_id = condition.id rescue nil
 
-  Trip.create(
-                  )
+  Trip.create(duration: row[:duration],
+              start_date: start_date,
+              end_date: end_date,
+              end_station_id: end_station_id,
+              bike_id: row[:bike_id],
+              zip_code: row[:zip_code],
+              station_id: start_station_id,
+              condition_id: condition_id,
+              subscription_type_id: subscription_type.id
+              )
 end
