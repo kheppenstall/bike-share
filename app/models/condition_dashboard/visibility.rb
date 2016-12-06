6c44bb48aa96a467635557dbcbcfa491caa7ddca
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-12-06 16:42:06 -0700

    Condition dashboard methods finished.

--- app/models/condition_dashboard/visibility.rb
+++ app/models/condition_dashboard/visibility.rb
@@ -1,6 +1,25 @@
 module Visibility
 
+  def conditions_from_visibility(visibility_floor)
+    all.find_all do |condition|
+      speed = condition.mean_visibility
+      speed >= visibility_floor && speed < visibility_floor + 4
+    end
+  end
+
+  def average_number_of_rides_from_visibility(visibility_floor)
+    conditions = conditions_from_visibility(visibility_floor)
+    average_rides(conditions)
+  end
+
+  def highest_number_of_rides_from_visibility(visibility_floor)
+    conditions = conditions_from_visibility(visibility_floor)
+    highest_number_of_rides(conditions)
+  end
+
+  def lowest_number_of_rides_from_visibility(visibility_floor)
+    conditions = conditions_from_visibility(visibility_floor)
+    lowest_number_of_rides(conditions)
+  end
+  
 end
-
-
-# Breakout of average number of rides, highest number of rides, and lowest number of rides on days with mean visibility in miles in four mile increments.
