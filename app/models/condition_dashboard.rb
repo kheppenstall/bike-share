35d6eb8e7a13e6c6f76fa98268e58f637e311120
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-12-06 15:43:00 -0700

    Halfway done with condition dashboard methods.

--- app/models/condition_dashboard.rb
+++ app/models/condition_dashboard.rb
@@ -2,41 +2,79 @@ require_relative 'names'
 
 module ConditionDashboard
   
-  def range(temp_floor)
-    (temp_floor..temp_floor + 9)
-  end
-
-  def conditions_from_temperature(temp_floor)
-    Condition.all.find_all{|condition| range(temp_floor).include?(condition.max_temperature)}
-  end
-
   def trips_from_conditions(conditions)
     condition_ids = conditions.map {|condition| condition.id}
     Trip.where(condition_ids.include?(:condition_id))
   end
   
-  def average_number_of_rides_from_temp(temp_floor, date)
-    conditions = conditions_from_temperature(temp_floor)
+  def average_rides(conditions)
     return 0 if conditions.count.zero?
-
     trips = trips_from_conditions(conditions)
     (trips.count / conditions.count.to_f).round(1)
   end
 
-  def highest_number_of_rides_from_temp(temp_floor, date)
-    conditions = conditions_from_temperature(temp_floor)
+  def highest_number_of_rides(conditions)
+    return 0 if conditions.count.zero?
     max_condition = conditions.max_by {|condition| condition.trips.count}
     max_condition.trips.count
   end
 
+  def lowest_number_of_rides(conditions)
+    return 0 if conditions.count.zero?
+    min_condition = conditions.min_by {|condition| condition.trips.count}
+    min_condition.trips.count
+  end
+  
+  def temp_range(temp_floor)
+    (temp_floor..temp_floor + 9)
+  end
+
+  def conditions_from_temperature(temp_floor)
+    all.find_all{|condition| temp_range(temp_floor).include?(condition.max_temperature)}
+  end
+
+  def average_number_of_rides_from_temp(temp_floor)
+    conditions = conditions_from_temperature(temp_floor)
+    average_rides(conditions)
+  end
+
+  def highest_number_of_rides_from_temp(temp_floor)
+    conditions = conditions_from_temperature(temp_floor)
+    highest_number_of_rides(conditions)
+  end
+
+  def lowest_number_of_rides_from_temp(temp_floor)
+    conditions = conditions_from_temperature(temp_floor)
+    lowest_number_of_rides(conditions)
+  end
+
+  def conditions_from_precip(precip_ceil)
+    ceil = precip_ceil.to_f
+    return all.where(precipitation: 0) if ceil == 0
+    all.find_all do |condition|
+      condition.precipitation <= ceil && condition.precipitation > ceil - 0.5
+    end
+  end
+
+  def average_number_of_rides_from_precip(precip_ceil)
+    conditions = conditions_from_precip(precip_ceil)
+    average_rides(conditions)
+  end
+
+  def highest_number_of_rides_from_precip(precip_ceil)
+    conditions = conditions_from_precip(precip_ceil)
+    highest_number_of_rides(conditions)
+  end
+
+  def lowest_number_of_rides_from_precip(precip_ceil)
+    conditions = conditions_from_precip(precip_ceil)
+    lowest_number_of_rides(conditions)
+  end
+
+
 end
 
 
-# Breakout of average number of rides, highest number of rides, 
-# and lowest number of rides on days with a high temperature 
-# in 10 degree chunks (e.g. average number of rides on days with 
-# high temps between fifty and sixty degrees)
 
-# Breakout of average number of rides, highest number of rides, and lowest number of rides on days with precipitation in half-inch increments.
 # Breakout of average number of rides, highest number of rides, and lowest number of rides on days with mean wind speeds in four mile increments.
 # Breakout of average number of rides, highest number of rides, and lowest number of rides on days with mean visibility in miles in four mile increments.
--- spec/models/conditions/condition_dashboard_spec.rb
+++ spec/models/conditions/condition_dashboard_spec.rb
@@ -7,12 +7,12 @@ describe 'Condition dashboard' do
       condition = Condition.create(date: "01/01/2015", max_temperature: 55)
       trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
 
-      expect(Condition.average_number_of_rides_from_temp(50, condition.date)).to eq(0.5)
+      expect(Condition.average_number_of_rides_from_temp(50)).to eq(0.5)
     end
 
     it 'assigns average numer of rides to zero when there are no trips in range' do
       
-      expect(Condition.average_number_of_rides_from_temp(0, "01/01/2015")).to eq(0)
+      expect(Condition.average_number_of_rides_from_temp(0)).to eq(0)
     end
 
     it 'calculates average number of trips only including those in range' do
@@ -20,7 +20,7 @@ describe 'Condition dashboard' do
       condition = Condition.create(date: "01/01/2015", max_temperature: 55)
       trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
 
-      expect(Condition.average_number_of_rides_from_temp(50, condition.date)).to eq(1)
+      expect(Condition.average_number_of_rides_from_temp(50)).to eq(1)
     end
   end
 
@@ -29,7 +29,178 @@ describe 'Condition dashboard' do
       condition = Condition.create(date: "01/01/2015", max_temperature: 55)
       trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
 
-      expect(Condition.highest_number_of_rides_from_temp(50, condition.date)).to eq(1)
+      expect(Condition.highest_number_of_rides_from_temp(50)).to eq(1)
+    end
+
+    it 'finds the highest number of rides on any day within the temperature range with two trips' do
+      condition = Condition.create(date: "01/01/2015", max_temperature: 55)
+      trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+      trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.highest_number_of_rides_from_temp(50)).to eq(2)
+    end
+
+    it 'finds the highest number of rides on any day within the temperature range with two conditions' do
+      condition_one = Condition.create(date: "01/01/2015", max_temperature: 55)
+      condition_two = Condition.create(date: "01/02/2015", max_temperature: 55)
+      trip = condition_one.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+      trip = condition_two.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.highest_number_of_rides_from_temp(50)).to eq(1)
+    end
+
+    it 'finds the highest number of rides on a day with no condition' do
+
+      expect(Condition.highest_number_of_rides_from_temp(50)).to eq(0)
+    end
+  end
+
+  describe '.lowest_number_of_rides_from_temp' do
+    it 'finds the lowest number of rides on any day within the temperature range where is only one trip' do
+      condition = Condition.create(date: "01/01/2015", max_temperature: 55)
+      trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.lowest_number_of_rides_from_temp(50)).to eq(1)
+    end
+
+    it 'finds the lowest number of rides on any day within the temperature range with two trips' do
+      condition = Condition.create(date: "01/01/2015", max_temperature: 55)
+      trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+      trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.lowest_number_of_rides_from_temp(50)).to eq(2)
+    end
+
+    it 'finds the lowest number of rides on any day within the temperature range with two conditions' do
+      condition_one = Condition.create(date: "01/01/2015", max_temperature: 55)
+      condition_two = Condition.create(date: "01/02/2015", max_temperature: 55)
+      trip = condition_one.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+      trip = condition_two.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.lowest_number_of_rides_from_temp(50)).to eq(1)
+    end
+
+    it 'finds the lowest number of rides on a day with no condition' do
+
+      expect(Condition.lowest_number_of_rides_from_temp(50)).to eq(0)
+    end
+  end
+
+  describe '.average_number_of_rides_from_precip' do
+    it 'calculates average number of trips given floor of precip chunk' do
+      condition = Condition.create(date: "01/01/2015", precipitation: 0)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.average_number_of_rides_from_precip(0)).to eq(2)
+    end
+
+    it 'calculates average number of trips given floor of precip chunk greater than zero' do
+      condition = Condition.create(date: "01/01/2015", precipitation: 0.8)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.average_number_of_rides_from_precip(1)).to eq(2)
+    end
+
+    it 'calculates average number of trips with multiple conditions' do
+      condition = Condition.create(date: "01/01/2015", precipitation: 0.7)
+      Condition.create(date: "01/02/2015", precipitation: 0.8)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.average_number_of_rides_from_precip(1)).to eq(0.5)
+    end
+
+    it 'assigns average numer of rides to zero when there are no trips in precip range' do
+      
+      expect(Condition.average_number_of_rides_from_precip(0)).to eq(0)
+    end
+
+    it 'calculates average number of trips only including those in precip range' do
+      Condition.create(date: "01/02/2015", precipitation: 0.4)
+      condition = Condition.create(date: "01/01/2015", precipitation: 0.8)
+      trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.average_number_of_rides_from_precip(0.5)).to eq(1)
+    end
+  end
+
+  describe '.highest_number_of_rides_from_precip' do
+    it 'calculates highest number of trips given floor of precip chunk' do
+      condition = Condition.create(date: "01/01/2015", precipitation: 0)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.highest_number_of_rides_from_precip(0)).to eq(2)
+    end
+
+    it 'calculates highest number of trips given floor of precip chunk greater than zero' do
+      condition = Condition.create(date: "01/01/2015", precipitation: 0.7)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.highest_number_of_rides_from_precip(1)).to eq(1)
+    end
+
+    it 'calculates highest number of trips with multiple conditions' do
+      condition = Condition.create(date: "01/01/2015", precipitation: 0.7)
+      Condition.create(date: "01/02/2015", precipitation: 0.8)
+
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.highest_number_of_rides_from_precip(1)).to eq(1)
+      expect(Condition.highest_number_of_rides_from_precip(0.5)).to eq(0)
+    end
+
+    it 'assigns highest numer of rides to zero when there are no trips in precip range' do
+      
+      expect(Condition.highest_number_of_rides_from_precip(0)).to eq(0)
+    end
+
+    it 'calculates highest number of trips only including those in precip range' do
+      Condition.create(date: "01/02/2015", precipitation: 0.4)
+      condition = Condition.create(date: "01/01/2015", precipitation: 0.8)
+      trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.highest_number_of_rides_from_precip(0.5)).to eq(0)
+    end
+  end
+
+  describe '.lowest_number_of_rides_from_precip' do
+    it 'calculates lowest number of trips given floor of precip chunk' do
+      condition = Condition.create(date: "01/01/2015", precipitation: 0)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.lowest_number_of_rides_from_precip(0)).to eq(2)
+    end
+
+    it 'calculates lowest number of trips given floor of precip chunk greater than zero' do
+      condition = Condition.create(date: "01/01/2015", precipitation: 0.7)
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.lowest_number_of_rides_from_precip(1)).to eq(1)
+    end
+
+    it 'calculates lowest number of trips with multiple conditions' do
+      condition = Condition.create(date: "01/01/2015", precipitation: 0.7)
+      Condition.create(date: "01/02/2015", precipitation: 0.8)
+
+      condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.lowest_number_of_rides_from_precip(1)).to eq(0)
+    end
+
+    it 'assigns lowest numer of rides to zero when there are no trips in precip range' do
+      
+      expect(Condition.lowest_number_of_rides_from_precip(0)).to eq(0)
+    end
+
+    it 'calculates lowest number of trips only including those in precip range' do
+      Condition.create(date: "01/02/2015", precipitation: 0.4)
+      condition = Condition.create(date: "01/01/2015", precipitation: 0.8)
+      trip = condition.trips.create(duration: 0, start_date: "01/01/2015", end_date: "01/01/2015", end_station_id: 0, bike_id: 0, zip_code: 0, station_id: 0, subscription_type_id: 0)
+
+      expect(Condition.lowest_number_of_rides_from_precip(1)).to eq(1)
     end
   end
 end
