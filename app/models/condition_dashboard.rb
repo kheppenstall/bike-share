4074ba90e8805d6f02e7ed31de3ab9fbcf4ff363
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-12-06 18:53:57 -0700

    Refactor condition dashboard methods to use active record methods.

--- app/models/condition_dashboard.rb
+++ app/models/condition_dashboard.rb
@@ -10,26 +10,34 @@ module ConditionDashboard
   include WindSpeed
   include Temperature
   
-  def trips_from_conditions(conditions)
-    condition_ids = conditions.map {|condition| condition.id}
-    Trip.where(condition_ids.include?(:condition_id))
+  # def conditions_by_trip
+  #   Condition.joins(:trips).group(:date).count("id")
+  # end
+
+  def average(array)
+    (array.reduce(:+).to_f / array.count).round(1)
   end
+
+  # def trips_from_conditions(conditions)
+  #   condition_ids = conditions.map {|condition| condition.id}
+  #   Trip.where(condition_ids.include?(:condition_id))
+  # end
   
-  def average_rides(conditions)
-    return 0 if conditions.count.zero?
-    trips = trips_from_conditions(conditions)
-    (trips.count / conditions.count.to_f).round(1)
-  end
+  # def average_rides(conditions)
+  #   return 0 if conditions.count.zero?
+  #   trips = trips_from_conditions(conditions)
+  #   (trips.count / conditions.count.to_f).round(1)
+  # end
 
-  def highest_number_of_rides(conditions)
-    return 0 if conditions.count.zero?
-    max_condition = conditions.max_by {|condition| condition.trips.count}
-    max_condition.trips.count
-  end
+  # def highest_number_of_rides(conditions)
+  #   return 0 if conditions.count.zero?
+  #   max_condition = conditions.max_by {|condition| condition.trips.count}
+  #   max_condition.trips.count
+  # end
 
-  def lowest_number_of_rides(conditions)
-    return 0 if conditions.count.zero?
-    min_condition = conditions.min_by {|condition| condition.trips.count}
-    min_condition.trips.count
-  end
+  # def lowest_number_of_rides(conditions)
+  #   return 0 if conditions.count.zero?
+  #   min_condition = conditions.min_by {|condition| condition.trips.count}
+  #   min_condition.trips.count
+  # end
 end
--- app/models/condition_dashboard/visibility.rb
+++ app/models/condition_dashboard/visibility.rb
@@ -1,25 +1,23 @@
 module Visibility
 
-  def conditions_from_visibility(visibility_floor)
-    all.find_all do |condition|
-      speed = condition.mean_visibility
-      speed >= visibility_floor && speed < visibility_floor + 4
-    end
+  def range(floor)
+    [floor..floor + 4]
   end
 
-  def average_number_of_rides_from_visibility(visibility_floor)
-    conditions = conditions_from_visibility(visibility_floor)
-    average_rides(conditions)
+  def visibility_trips(floor)
+   conditions = where(mean_visibility: range(floor))
+   conditions.joins(:trips).group(:date).count("id")
   end
 
-  def highest_number_of_rides_from_visibility(visibility_floor)
-    conditions = conditions_from_visibility(visibility_floor)
-    highest_number_of_rides(conditions)
+  def average_number_of_rides_from_visibility(floor)
+    average(visibility_trips(floor).values)
   end
 
-  def lowest_number_of_rides_from_visibility(visibility_floor)
-    conditions = conditions_from_visibility(visibility_floor)
-    lowest_number_of_rides(conditions)
+  def highest_number_of_rides_from_visibility(floor)
+    visibility_trips(floor).values.max
   end
-  
+
+  def lowest_number_of_rides_from_visibility(floor)
+    visibility_trips(floor).values.min
+  end
 end
