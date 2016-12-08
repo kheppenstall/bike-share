40ccb92818eb70a9150a0638818ef7460ffbbfb0
Daleon <rugfia@yahoo.com>
2016-12-07 19:32:13 -0700

    Re-Seeded after changing seed.rb file

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -9,8 +9,4 @@ class Condition < ActiveRecord::Base
 
   include NextPrevious
   extend ConditionDashboard
-
-  def weather_information
-    "#{date}, #{max_temperature}, #{mean_temperature}, #{min_temperature}, #{mean_humidity}, #{mean_visibility}, #{mean_wind_speed}, #{precipitation}"
-  end
 end
